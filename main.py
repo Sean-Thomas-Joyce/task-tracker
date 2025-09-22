d1a8bcfeb1eee9e97cdf0969fa979146198fb9f5
Sean Joyce <seanthomasjoyce@icloud.com>
2025-09-22 11:15:01 +0100

    Refactor task management functions and update task descriptions

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# Todo: Update task
-# Todo: Delete Task
-# Todo: Change status of task
 from dataclasses import asdict
 from datetime import datetime
 import typer
@@ -17,18 +14,40 @@ path = Path("tasks.json")
 
 
 @app.command()
-def add(title: str):
+def add(description: str):
     id = max((t.id for t in tasks), default=0) + 1
-    tasks.append(Task(id, title))
-    contents = json.dumps([asdict(task) for task in tasks], default=custom_encoder)
-    path.write_text(contents)
-    print(f"Added task: {title}")
+    tasks.append(Task(id, description))
+    write_tasks()
+    print(f"Added task: {description}")
+
+
+@app.command()
+def update(id: int, description: str):
+    task = next((t for t in tasks if t.id == id), None)
+    if task:
+        task.update_description(description)
+        write_tasks()
+    else:
+        print(f"Cant find task with id {id}")
 
 
 @app.command()
 def list():
     for task in tasks:
         print(task.__str__())
+
+
+@app.command()
+def delete(id: int):
+    global tasks
+    tasks = [task for task in tasks if task.id != id]
+    write_tasks()
+    print(f"Deleted task with id {id}")
+
+
+def write_tasks():
+    contents = json.dumps([asdict(task) for task in tasks], default=custom_encoder)
+    path.write_text(contents)
 
 
 def load_tasks():
--- task.py
+++ task.py
@@ -17,5 +17,9 @@ class Task:
     created_at: date = datetime.now()
     updated_at: date = datetime.now()
 
+    def update_description(self, new_description: str):
+        self.description = new_description
+        self.updated_at = datetime.now()
+
     def __str__(self):
         return f"[{self.id}] {self.description} - {self.status.name} (Created: {self.created_at}, Updated: {self.updated_at})"
