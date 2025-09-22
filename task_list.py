727c7e5d7cdfe2988e9ae424843ee647beca4a30
Sean Joyce <seanthomasjoyce@icloud.com>
2025-09-22 17:33:50 +0100

    Refactor task path handling to use instance variable and format JSON for better readability

--- task_list.py
+++ task_list.py
@@ -4,21 +4,20 @@ from pydantic import BaseModel
 
 from task import Task
 
-path = Path("tasks.json")
-
 
 class TaskList(BaseModel):
     tasks: list[Task] = []
+    path = Path("tasks.json")
 
     def load_tasks(self):
-        if path.exists():
-            loaded_tasklist = TaskList.model_validate_json(path.read_text())
+        if self.path.exists():
+            loaded_tasklist = TaskList.model_validate_json(self.path.read_text())
             self.tasks = loaded_tasklist.tasks
 
     def add_task(self, description: str):
         task_id = max((t.id for t in self.tasks), default=0) + 1
         self.tasks.append(Task(id=task_id, description=description))
-        path.write_text(self.model_dump_json())
+        self.path.write_text(self.model_dump_json())
         print(f"Added task: {description}")
 
     def update_task(self, id: int, description: str):
@@ -27,13 +26,13 @@ class TaskList(BaseModel):
             task.description = description
             task.updated_at = datetime.now()
             print(f"Updated task {id} description to: {description}")
-            path.write_text(self.model_dump_json())
+            self.path.write_text(self.model_dump_json())
         else:
             print(f"Cant find task with id {id}")
 
     def delete_task(self, id: int):
         self.tasks = [task for task in self.tasks if task.id != id]
-        path.write_text(self.model_dump_json())
+        self.path.write_text(self.model_dump_json())
         print(f"Deleted task with id {id}")
 
     def print_tasks(self):
