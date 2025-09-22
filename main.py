5b50344708aa51f3049a350c32353fa179cf9489
Sean Joyce <seanthomasjoyce@icloud.com>
2025-09-22 11:36:21 +0100

    Refactor task management by creating TaskList class and updating main application logic

--- main.py
+++ main.py
@@ -1,75 +1,43 @@
-from dataclasses import asdict
-from datetime import datetime
 import typer
-import json
 from pathlib import Path
 
-from task import Status, Task
-from util.custom_encoder import custom_encoder
+from task_list import TaskList
 
 app = typer.Typer()
 
-tasks: list[Task] = []
 path = Path("tasks.json")
+
+task_list = TaskList()
 
 
 @app.command()
 def add(description: str):
-    id = max((t.id for t in tasks), default=0) + 1
-    tasks.append(Task(id, description))
-    write_tasks()
+    task_list.add_task(description)
+    task_list.write_tasks()
     print(f"Added task: {description}")
 
 
 @app.command()
 def update(id: int, description: str):
-    task = next((t for t in tasks if t.id == id), None)
-    if task:
-        task.update_description(description)
-        write_tasks()
-    else:
-        print(f"Cant find task with id {id}")
+    updated = task_list.update_task(id, description)
+    if updated:
+        task_list.write_tasks()
 
 
 @app.command()
 def list():
-    for task in tasks:
-        print(task.__str__())
+    task_list.print_tasks()
 
 
 @app.command()
 def delete(id: int):
-    global tasks
-    tasks = [task for task in tasks if task.id != id]
-    write_tasks()
+    task_list.delete_task(id)
+    task_list.write_tasks()
     print(f"Deleted task with id {id}")
 
 
-def write_tasks():
-    contents = json.dumps([asdict(task) for task in tasks], default=custom_encoder)
-    path.write_text(contents)
-
-
-def load_tasks():
-    global tasks
-    if path.exists():
-        contents = path.read_text()
-        data = json.loads(contents)
-        tasks.clear()
-        for item in data:
-            tasks.append(
-                Task(
-                    id=item["id"],
-                    description=item["description"],
-                    status=Status(item["status"]),
-                    created_at=datetime.fromisoformat(item["created_at"]),
-                    updated_at=datetime.fromisoformat(item["updated_at"]),
-                )
-            )
-
-
 def main():
-    load_tasks()
+    task_list.load_tasks()
     app()
 
 
---
+++ task_list.py
@@ -0,0 +1,57 @@
+from dataclasses import dataclass, asdict
+from datetime import datetime
+from pathlib import Path
+import json
+
+from task import Status, Task
+from util.custom_encoder import custom_encoder
+
+
+path = Path("tasks.json")
+
+
+@dataclass
+class TaskList:
+    tasks: list[Task] = []
+
+    def load_tasks(self):
+        if path.exists():
+            contents = path.read_text()
+            data = json.loads(contents)
+            self.tasks.clear()
+            for item in data:
+                self.tasks.append(
+                    Task(
+                        id=item["id"],
+                        description=item["description"],
+                        status=Status(item["status"]),
+                        created_at=datetime.fromisoformat(item["created_at"]),
+                        updated_at=datetime.fromisoformat(item["updated_at"]),
+                    )
+                )
+
+    def add_task(self, description: str):
+        id = max((t.id for t in self.tasks), default=0) + 1
+        self.tasks.append(Task(id, description))
+
+    def update_task(self, id: int, description: str) -> bool:
+        task = next((t for t in self.tasks if t.id == id), None)
+        if task:
+            task.update_description(description)
+            return True
+        else:
+            print(f"Cant find task with id {id}")
+            return False
+
+    def delete_task(self, id: int):
+        self.tasks = [task for task in self.tasks if task.id != id]
+
+    def print_tasks(self):
+        for task in self.tasks:
+            print(task.__str__())
+
+    def write_tasks(self):
+        contents = json.dumps(
+            [asdict(task) for task in self.tasks], default=custom_encoder
+        )
+        path.write_text(contents)
